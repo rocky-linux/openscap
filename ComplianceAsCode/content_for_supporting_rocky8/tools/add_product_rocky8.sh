358b7640c415df24bdba425ea6ad00ecc51d9deb
Scott R. Shinn <scott@atomicorp.com>
2021-08-21 15:55:56 -0400

    Rebasing on rl8
    Adding spec's for main and 0.1.54 branches
    
    Signed-off-by: Scott R. Shinn <scott@atomicorp.com>

--- ComplianceAsCode/content_for_supporting_rocky8/tools/add_product_rocky8.sh
+++ ComplianceAsCode/content_for_supporting_rocky8/tools/add_product_rocky8.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 ### copy rocky8 directory under new directory
-cp -pr ./content_for_supporting_rocky8/files/rocky8 ./products/
+cp -pr ./content_for_supporting_rocky8/files/rl8 ./products/ || exit 1
 ### copy several files
-cp -pr ./content_for_supporting_rocky8/files/installed_OS_is_rocky8.xml ./shared/checks/oval/
-cp -pr ./content_for_supporting_rocky8/files/disa-stig-rocky8-v1r3-xccdf-manual.xml ./shared/references/
+#cp -pr ./content_for_supporting_rocky8/files/installed_OS_is_rocky8.xml ./shared/checks/oval/
+cp -pr ./content_for_supporting_rocky8/files/installed_OS_is_rl8.xml ./shared/checks/oval/
+cp -pr ./content_for_supporting_rocky8/files/disa-stig-rl8-v1r3-xccdf-manual.xml ./shared/references/
 
 ### patch to several files for supporting rocky8
 patch -p1 < ./content_for_supporting_rocky8/files/diff_content_for_supporting_rocky8
 
 ### add rocky8 to several XML definition files.
-find ./linux_os -type f -exec sed -i '/prodtype:/s/rhel8/rhel8,rocky8/g' {} \;
-find ./tests -type f -exec sed -i '/prodtype:/s/rhel8/rhel8,rocky8/g' {} \;
+find ./linux_os -type f -exec sed -i '/prodtype:/s/rhel8/rhel8,rl8/g' {} \;
+find ./tests -type f -exec sed -i '/prodtype:/s/rhel8/rhel8,rl8/g' {} \;
 find ./linux_os -type f -exec sed -i '/platform/s/multi_platform_rhel/multi_platform_rhel,multi_platform_rocky/g' {} \;
 find ./shared -type f -exec sed -i '/platform/s/multi_platform_rhel/multi_platform_rhel,multi_platform_rocky/g' {} \;
 find ./shared -type f -exec sed -i '/platform/s/Red Hat Enterprise Linux 8/Red Hat Enterprise Linux 8,multi_platform_rocky/g' {} \;
