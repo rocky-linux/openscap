11908df03d06230557d08585ad7dba964e92c33a
Scott R. Shinn <scott@atomicorp.com>
2021-10-16 14:17:19 -0400

    Updating for 0.1.57
    Signed-off-by: Scott R. Shinn <scott@atomicorp.com>

--- ComplianceAsCode/content_for_supporting_rocky8/tools/add_product_rocky8.sh
+++ ComplianceAsCode/content_for_supporting_rocky8/tools/add_product_rocky8.sh
@@ -3,12 +3,17 @@
 ### copy rocky8 directory under new directory
 cp -pr ./content_for_supporting_rocky8/files/rl8 ./products/ || exit 1
 ### copy several files
-#cp -pr ./content_for_supporting_rocky8/files/installed_OS_is_rocky8.xml ./shared/checks/oval/
 cp -pr ./content_for_supporting_rocky8/files/installed_OS_is_rl8.xml ./shared/checks/oval/
 cp -pr ./content_for_supporting_rocky8/files/disa-stig-rl8-v1r3-xccdf-manual.xml ./shared/references/
 
 ### patch to several files for supporting rocky8
-patch -p1 < ./content_for_supporting_rocky8/files/diff_content_for_supporting_rocky8
+patch -p1 < ./content_for_supporting_rocky8/files/rocky8-0.1.57.patch
+if [ $? -ne 0 ]; then
+	echo
+	echo "ERROR: patching failed"
+	echo
+	exit 1
+fi
 
 ### add rocky8 to several XML definition files.
 find ./linux_os -type f -exec sed -i '/prodtype:/s/rhel8/rhel8,rl8/g' {} \;
